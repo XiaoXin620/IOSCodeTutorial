989f5e8c93a4a18bca55df5ac07a7c521465639c
hexin <786019030@qq.com>
2021-07-29 00:07:58 +0800

    designcode 25

--- NothingCode/Home.swift
+++ NothingCode/Home.swift
@@ -10,15 +10,23 @@ import SwiftUI
 struct Home: View {
     @State var showProfile = false
     @State var viewState = CGSize.zero
+    @State var showContent = false
     
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/) //content 动画被这个覆盖了 没有找到解决方案
             
-            HomeView(showProfile: $showProfile)
+            HomeView(showProfile: $showProfile,showContent: $showContent)
                 .padding(.top, 44)
-                .background(Color.white)
+                .background(
+                    VStack {
+                        LinearGradient(gradient: Gradient(colors: [Color("background2"), Color.white]), startPoint: .top, endPoint: .bottom)
+                            .frame(height: 200)
+                        Spacer()
+                    }
+                    .background(Color.white)
+                )
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                 .offset(y: showProfile ? -450 : 0)
@@ -26,6 +34,33 @@ struct Home: View {
                 .scaleEffect(showProfile ? 0.9 : 1)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
                 .edgesIgnoringSafeArea(.all)
+            
+            // 不知道为啥 ios14 放这里才有效
+            if showContent {
+                Color.white.edgesIgnoringSafeArea(.all)
+                
+                ContentView()
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "xmark")
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    Spacer()
+                }
+                .offset(x: -16, y: 16)
+                .transition(.move(edge: .top)) // 设置过渡 从哪里出来 move
+                .animation(.spring(response: 0.6, dampingFraction: 0.8, blendDuration: 0))
+                .onTapGesture {
+                    self.showContent = false
+                }
+                
+            }
             
             MenuView()
                 .background(Color.black.opacity(0.001)) // 必须给颜色（哪怕是透明的） 不然没有 事件产生
--- NothingCode/HomeView.swift
+++ NothingCode/HomeView.swift
@@ -10,76 +10,97 @@ import SwiftUI
 struct HomeView: View {
     @Binding var showProfile: Bool
     @State var showUpdate = false
+    @Binding var showContent: Bool // 想要在 Home视图中控制
     
     var body: some View {
-        VStack {
-            HStack {
-                Text("Watching")
-//                    .font(.system(size: 28, weight: .bold))
-                    .modifier(CustomFontModifier(size: 28)) // 使用自定义字体
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Watching")
+    //                    .font(.system(size: 28, weight: .bold))
+                        .modifier(CustomFontModifier(size: 28)) // 使用自定义字体
+                    
+                    Spacer()
+                    
+                    AvatarView(showProfile: $showProfile) // 向子组件传值需要 $
+                    
+                    Button(action: { self.showUpdate.toggle() }) {
+                        Image(systemName: "bell")
+                            .renderingMode(.original)
+                            .font(.system(size: 16 , weight: .medium))
+                            .frame(width: 36, height: 36)
+                            .background(Color.white)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            // 双重投影 相当于现实生活中两个光源
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                    }
+                    // 展开新视图 ios13+ 一个卡片一样的视图 用来快速获取信息使用的
+                    .sheet(isPresented: $showUpdate, content: {
+                        UpdateList()
+                    })
+                }
+                .padding(.horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    WatchRingView()
+                        .padding(.horizontal, 30)
+                        .padding(.bottom, 30)
+                        .onTapGesture {
+                            self.showContent = true
+                        }
+                }
+                
+                
+                // showsIndicators 滚动指示器显示
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(sectionData) { item in
+                            GeometryReader { geometry in
+                                SectionView(section: item)
+                                    // 这个不懂 15课程
+                                    .rotation3DEffect(Angle(degrees:
+                                                               
+                                          Double(geometry.frame(in: .global).minX - 30 ) / -20
+                                    ), axis: (x: 0, y: 10, z: 0))
+                            }
+                            .frame(width: 275, height: 275) //设置框架大小
+                        }
+                    }
+                    .padding(30)
+                    .padding(.leading, 14)
+                    .padding(.bottom, 30)
+                }
+                .offset(y: -30) // 无法使上一个滚动视图更改间距 只能使用偏移
+                
+                HStack {
+                    Text("Courses")
+                        .font(.title).bold()
+                    
+                    Spacer()
+                }
+                .padding(.leading, 30)
+                .offset(y: -60)
+                
+                SectionView(section: sectionData[2], width: screen.width - 60, height: 275)
+                    .offset(y: -60)
                 
                 Spacer()
-                
-                AvatarView(showProfile: $showProfile) // 向子组件传值需要 $
-                
-                Button(action: { self.showUpdate.toggle() }) {
-                    Image(systemName: "bell")
-                        .renderingMode(.original)
-                        .font(.system(size: 16 , weight: .medium))
-                        .frame(width: 36, height: 36)
-                        .background(Color.white)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                        // 双重投影 相当于现实生活中两个光源
-                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
-                }
-                // 展开新视图 ios13+ 一个卡片一样的视图 用来快速获取信息使用的
-                .sheet(isPresented: $showUpdate, content: {
-                    UpdateList()
-                })
             }
-            .padding(.horizontal)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                WatchRingView()
-                    .padding(.horizontal, 30)
-                    .padding(.bottom, 30)
-            }
-            
-            
-            // showsIndicators 滚动指示器显示
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) {
-                    ForEach(sectionData) { item in
-                        GeometryReader { geometry in
-                            SectionView(section: item)
-                                // 这个不懂 15课程
-                                .rotation3DEffect(Angle(degrees:
-                                                           
-                                      Double(geometry.frame(in: .global).minX - 30 ) / -20
-                                ), axis: (x: 0, y: 10, z: 0))
-                        }
-                        .frame(width: 275, height: 275) //设置框架大小
-                    }
-                }
-                .padding(30)
-                .padding(.leading, 14)
-                .padding(.bottom, 30)
-            }
-            
-            Spacer()
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(showProfile: .constant(true))
+        HomeView(showProfile: .constant(true),showContent: .constant(true))
     }
 }
 
 struct SectionView: View {
     var section: Section
+    var width: CGFloat = 275
+    var height: CGFloat = 275
     
     var body: some View {
         VStack {
@@ -105,7 +126,7 @@ struct SectionView: View {
         }
         .padding(.top, 20)
         .padding(.horizontal,20)
-        .frame(width: 275, height: 275)
+        .frame(width: width, height: height)
         .background(section.color)
         .cornerRadius(30)
         .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
@@ -124,7 +145,7 @@ struct Section: Identifiable {
 
 let sectionData = [
     Section(title: "Prototype design in SwiftUI", text: "18 Section", logo: "Logo1", image: Image("Card1"), color: Color("card1")),
-    Section(title: "Build a SwiftUI app", text: "20 Section", logo: "Logo2", image: Image(uiImage: #imageLiteral(resourceName: "Card1")), color: Color("card2")),
+    Section(title: "Build a SwiftUI app", text: "20 Section", logo: "Logo2", image: Image(uiImage: #imageLiteral(resourceName: "Logo1")), color: Color("card2")),
     Section(title: "SwiftUI Advanced", text: "20 Section", logo: "Logo3", image: Image("Card3"), color: Color("card3"))
 ]
 
